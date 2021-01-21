87205057f1765e1b183a848bf66e1ba5556a631a
Ninten Gamer <20716219+NintenZone@users.noreply.github.com>
2021-01-20 19:27:01 -0700

    Init

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Import NPM Packages
 const express = require('express');
+const fs = require('fs');
 
 //Import Config
 const config = require('./config.js');
@@ -11,6 +12,12 @@ const port = config.port;
 //Get Auth Info 
 const keys = config.keys;
 
+//Function to save data to .json file
+const saveData = function(data) {
+    data = JSON.stringify(data, null, 2);
+    fs.writeFileSync('./data/data.json', data);
+};
+
 app.use(express.json());
 app.post('/update/:auth', (req, res) => {
     let auth = req.params.auth;
@@ -18,8 +25,12 @@ app.post('/update/:auth', (req, res) => {
     if (auth && keys.includes(auth)) {
         if (!req.body) return res.sendStatus(400);
         console.log("[INFO]\tRequest from NodeCG was acknowledged and recorded.");
+        res.sendStatus(200);
+        
+        saveData(req.body);
 
     }
+
     else {
         console.warn("[WARN]\tA request was forbidden.")
         return res.sendStatus(403);
